e0ac4cbc6c397067eeb1bb1a76ce86e8fb2984cf
Sandeep Reddy <msande1996@gmail.com>
2021-03-25 03:27:48 +0530

    feat: add session and update ci to commit back cache

--- src/ig/ig_client.ts
+++ src/ig/ig_client.ts
@@ -5,7 +5,11 @@ import {
   IgApiClient,
 } from 'instagram-private-api';
 import { cache } from '../cache/cache';
+import { encryptor } from '../util/cipher';
+import * as path from 'path';
+import * as fs from 'fs';
 
+const sessionFile = path.join(__dirname, '..', '..', '..', 'xyz.encrypt');
 class IgClient {
   ig: IgApiClient;
   loggedInUser: AccountRepositoryLoginResponseLogged_in_user;
@@ -15,6 +19,19 @@ class IgClient {
 
   login = async () => {
     this.ig.state.generateDevice(process.env.IG_USERNAME);
+    //store session
+    this.ig.request.end$.subscribe(async () => {
+      const serialized = await this.ig.state.serialize();
+      delete serialized.constants;
+      const sessionData = encryptor.encrypt(JSON.stringify(serialized));
+      fs.writeFileSync(sessionFile, sessionData);
+    });
+    //restore session
+    if (fs.existsSync(sessionFile)) {
+      const encSessionData = fs.readFileSync(sessionFile).toString();
+      const sessionData = encryptor.decrypt(encSessionData);
+      await this.ig.state.deserialize(sessionData);
+    }
     this.loggedInUser = await this.ig.account.login(
       process.env.IG_USERNAME,
       process.env.IG_PASSWORD
